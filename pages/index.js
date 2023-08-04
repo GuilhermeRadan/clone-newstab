c18cb2d21fb860506a6322841f866a647959bac7
Radan <radanguilherme1@gmail.com>
2023-08-04 02:15:43 -0300

    adiciona script `lint:check`e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
-function Home(){
-    return <h1>hackeia aí otário! Saiu alguma coisa aqui!</h1> 
-
+function Home() {
+  return <h1>hackeia aí otário! Saiu alguma coisa aqui!</h1>;
 }
 
 export default Home;
